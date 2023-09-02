6d6b1bb206c0152c38dc343e9ff9cc9cb0fd63b7
Vladimir Fedosov <vladimir.fedosov@bmw.de>
2023-09-02 08:30:09 +0200

    before the refactoring

--- main.py
+++ main.py
@@ -9,71 +9,37 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from torch import nn as nn
 from torch.optim import AdamW
-
-from utils import test_policy_network, seed_everything, plot_stats, plot_action_probs
+from utils import seed_everything
+from parallel_env import *
 import cv2
+import multiprocessing
 
-env = gym.make('CartPole-v0')
-
-dims = env.observation_space.shape[0]
-actions = env.action_space.n
-
-print(f"State dimensions: {dims}. Actions: {actions}")
-print(f"Sample state: {env.reset()}")
+num_envs = os.cpu_count()
 
 
-policy_ff = nn.Sequential(nn.Linear(dims, 64), nn.Linear(64, 32), nn.Linear(32, actions), nn.Softmax(dim=-1))
-values_ff = nn.Sequential(nn.Linear(dims, 64), nn.Linear(64, 32), nn.Linear(32, 1))
+class PreprocessEnv(ParallelWrapper):
+    def __init__(self, env):
+        ParallelWrapper.__init__(self, env)
 
+    def reset(self):
+        state = self.venv.reset()
+        return torch.from_numpy(state).float()
 
-def get_episode(policy, gamma):
-    episode = []
-    state = env.reset()
+    def step_async(self, actions):
+        actions = actions.squeeze().numpy()
+        self.venv.step_async(actions)
 
-    while True:
-        state = torch.from_numpy(state)
-        act = policy(state)
-        next_state, reward, done, info = env.step(act)
+    def step_wait(self):
+        next_state, reward, done, info = self.venv.step_wait()
+        next_state = torch.from_numpy(next_state).float()
+        reward = torch.tensor(reward).unsqueeze(1).float()
+        done = torch.tensor(done).unsqueeze(1)
+        return next_state, reward, done, info
 
-        episode.append((state, act, reward))
-
-        if done:
-            return episode
-
-        state = next_state
-
-
-def create_batches(batch_size, amount, policy, gamma=0.99):
-    size = batch_size * amount
-    steps = []
-    episodes_count = 0
-
-    while len(steps) < size:
-        items = get_episode(policy, gamma)
-
-        G = 0.0
-
-        for state, act, reward in reversed(items):
-            G = gamma * G + reward
-            steps.append((state, act, G, reward))
-
-            if len(steps) == size:
-                break
-
-        episodes_count += 1
-
-    print (f"Average eposode size: {len(steps) / episodes_count}")
-
-    random.shuffle(steps)
-
-    batches = []
-
-    for i in range(0, size, batch_size):
-        batches.append(steps[i: i + batch_size])
-
-    return batches
-
-
+def create_env(env_name):
+    env = gym.make(env_name)
+    seed_everything(env)
+    return env
 
 def current_policy(state):
     actions_prob = policy_ff(state)
@@ -81,58 +47,62 @@ def current_policy(state):
     action = 0 if random.random() <= action_prob else 1
     return action
 
-
 def train_step(alpha, gamma=0.99):
     global policy_ff
 
-    batches = create_batches(64, 64, current_policy, gamma)
-    optim = AdamW(policy_ff.parameters(), lr=alpha)
+    policy_optim = AdamW(policy_ff.parameters(), lr=alpha)
+    values_optim = AdamW(values_ff.parameters(), lr=alpha)
 
-    for batch in batches:
-        optim.zero_grad()
-        rewards = []
-        entropias = []
+    for _ in tqdm(range(100)):
+        states = parallel_env.reset()
+        done_b = torch.zeros([num_envs, 1], dtype=torch.bool)
+        reswards = torch.zeros([num_envs, 1])
 
-        for state, action, G, reward in batch:
-            actions_probs = policy_ff(state)
-            log_prob = torch.log(actions_probs)
-            action_log_prob = log_prob[action]
-            #gamma_t = gamma ** step
-            entropy = torch.sum(log_prob * actions_probs)
-            entropias.append((entropy * 0.1).reshape(1, 1))
-            rewards.append((G * action_log_prob).reshape(1, 1))
+        while not done_b.all():
+            actions = policy_ff(states)
+            log_actions = torch.log(actions + 1e-6)
+            action = actions.multinomial(1).detach()
+            next_states, reward, done, _ = parallel_env.step(action)
 
-        rewards = torch.sum(torch.cat(rewards))
-        entropias = torch.sum(torch.cat(entropias))
-        loss = -rewards #- 0.001 * entropias
-        policy_ff.zero_grad()
-        loss.backward()
-        optim.step()
-        zero = numpy.array([1.0, 0.1, 0.0, 0.0])
-        zero = torch.from_numpy(zero).float()
-        #print(fast_forward(zero))
+            target = (gamma * values_ff(next_states) + reward)
+            advantage = (target - values_ff(states)) * ~done_b
+            done_b |= done
+            reswards += reward
 
+            loss_values = (advantage * advantage).mean()
+            values_optim.zero_grad()
+            loss_values.backward()
+            values_optim.step()
 
-for i in range(50):
+            entropy = torch.sum(log_actions * actions, dim=-1, keepdim=True) * ~done_b
+            loss_policy = -advantage.detach() * log_actions.gather(1, action) - 0.001 * entropy
+            loss_policy = loss_policy.mean()
+            policy_optim.zero_grad()
+            loss_policy.backward()
+            policy_optim.step()
+
+            states = next_states
+
+        # zero = numpy.array([1.0, 0.1, 0.0, 0.0])
+        # zero = torch.from_numpy(zero).float()
+        print(reswards.mean().item())
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+
+    env_fns = [lambda: create_env('CartPole-v1') for _ in range(num_envs)]
+    parallel_env = PreprocessEnv(ParallelEnv(env_fns))
+
+    dims = parallel_env.observation_space.shape[0]
+    actions = parallel_env.action_space.n
+    policy_ff = nn.Sequential(nn.Linear(dims, 64), nn.Linear(64, 32), nn.Linear(32, actions), nn.Softmax(dim=-1))
+    values_ff = nn.Sequential(nn.Linear(dims, 64), nn.Linear(64, 32), nn.Linear(32, 1))
+
+    print(f"State dimensions: {dims}. Actions: {actions}")
+
     train_step(0.0003)
 
-while True:
-    state = env.reset()
 
-    while True:
-        state = torch.from_numpy(state)
-        act = current_policy(state)
-        next_state, reward, done, info = env.step(act)
-
-        if done:
-            state = env.reset()
-            continue
-
-        img = env.render(mode='rgb_array')
-        cv2.imshow("asd", img)
-        cv2.waitKey(50)
-
-        state = next_state
 
 
 
