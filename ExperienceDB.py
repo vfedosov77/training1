a890d6080d9312dae8808b76a244f1c2eb6a658b
Vladimir Fedosov <vladimir.fedosov@bmw.de>
2023-11-27 18:18:22 +0100

    remove deterministic checking

--- ExperienceDB.py
+++ ExperienceDB.py
@@ -13,7 +13,20 @@ class ExperienceDB:
         self.reward: torch.Tensor = None
         self.dataset = None
 
+        self.input2output = dict()
+
     def add(self, input, next_state, done):
+        # for i in range(len(input)):
+        #     state_i = input[i]
+        #     next_state_i = next_state[i]
+        #     key = (state_i[0].item(), state_i[1].item(), state_i[2].item(), state_i[3].item(), state_i[4].item())
+        #     value = (next_state_i[0].item(), next_state_i[1].item(), next_state_i[2].item(), next_state_i[3].item())
+        #
+        #     if key in self.input2output:
+        #         assert self.input2output[key] == value
+        #     else:
+        #         self.input2output[key] = value
+
         assert self.state is None
 
         if self.inputs is None:
@@ -28,6 +41,17 @@ class ExperienceDB:
             self.dataset.tensors = (self.inputs, self.next_state, self.done)
 
     def add_ext(self, state, action, next_state, done, reward):
+        # for i in range(len(state)):
+        #     state_i = state[i]
+        #     next_state_i = next_state[i]
+        #     key = (state_i[0].item(), state_i[1].item(), state_i[2].item(), state_i[3].item(), action[i].item())
+        #     value = (next_state_i[0].item(), next_state_i[1].item(), next_state_i[2].item(), next_state_i[3].item())
+        #
+        #     if key in self.input2output:
+        #         assert self.input2output[key] == value
+        #     else:
+        #         self.input2output[key] = value
+
         if self.next_state is None:
             self.state = torch.clone(state)
             self.action = torch.clone(action)
