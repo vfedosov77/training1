d72e3080bc78370fd955495ad0a87fcb6b3ff0dd
Vladimir <vfedosov77@gmail.com>
2024-01-11 17:41:41 +0100

    Before to add the root question

--- ChatBot/Dialog.py
+++ ChatBot/Dialog.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 from ChatBot.Constants import *
 import re
+import functools
+
 
 class Dialog(tk.Tk):
     def __init__(self):
@@ -10,23 +12,29 @@ class Dialog(tk.Tk):
 
         self.info_provider = None
         self.lines2details = dict()
-        self.cur_line = 0
+        self.cur_log_line = 0
+        self.cur_chat_line = 0
 
         self.title("Please ask the question")
-        self.geometry("800x800")
+        self.geometry("1000x800")
+
+        self.l1 = tk.Label(text="Chat:")
+        self.l1.grid(row=0, column=0)
+        self.l2 = tk.Label(text="Log:")
+        self.l2.grid(row=0, column=2)
 
         self.entry = tk.Entry(self, width=40)
-        self.entry.grid(row=1, column=0, sticky="NSEW")
+        self.entry.grid(row=2, column=0, sticky="NSEW")
         self.entry.insert(0, "How the camera focus is processed?")
         self.submit_button = tk.Button(self, text="Submit", command=self.submit_query, width=5)
-        self.submit_button.grid(row=1, column=1)
+        self.submit_button.grid(row=2, column=1)
 
-        self.log = tk.Text(self, width=40)
-        self.log.grid(row=0, column=2, rowspan=2, sticky="NSEW")
+        self.log = tk.Text(self, width=60)
+        self.log.grid(row=1, column=2, rowspan=2, sticky="NSEW")
         self.log.tag_configure("color_tag", foreground="green")
 
         self.scrollbar = tk.Scrollbar(self)
-        self.scrollbar.grid(row=0, column=3, rowspan=2, sticky="NSW")
+        self.scrollbar.grid(row=1, column=3, rowspan=2, sticky="NSW")
         self.log.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.log.yview)
         # Bind the item click event
@@ -34,14 +42,16 @@ class Dialog(tk.Tk):
         self.item_to_highlight = None
 
         self.chat = tk.Text(self, width=400)
-        self.chat.grid(row=0, column=0, sticky="NSEW")
+        self.chat.grid(row=1, column=0, sticky="NSEW")
         self.chat_scrollbar = tk.Scrollbar(self)
-        self.chat_scrollbar.grid(row=0, column=1, sticky="NSW")
+        self.chat_scrollbar.grid(row=1, column=1, sticky="NSW")
         self.chat.config(yscrollcommand=self.chat_scrollbar.set)
         self.chat_scrollbar.config(command=self.chat.yview)
+        self.chat.bind('<Double-1>', self.show_details)
+        self.chat.tag_configure("bold_tag", font=("TkDefaultFont", 12, "bold"))
 
         self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
 
         #self.add_log_entry("Response: The code contains the closely related part to the question. The method related to processing the camera focus is the `autoFocus()` method in the `RasterCapturer` class.", None)
 
@@ -49,7 +59,7 @@ class Dialog(tk.Tk):
         self.log.delete("1.0", tk.END)
 
         self.lines2details.clear()
-        self.cur_line = 0
+        self.cur_log_line = 0
 
 
     def set_provider(self, info_provider):
@@ -63,6 +73,11 @@ class Dialog(tk.Tk):
             self.submit_button.config(state=tk.DISABLED)
             self.clean()
 
+            self.chat.insert('end', "User: " + text + "\n")
+            self.set_tag(self.chat, 'bold_tag', self.cur_chat_line, 0, 5)
+            self.update()
+            self.cur_chat_line += 1
+
             try:
                 self.info_provider.get_answer(text)
             finally:
@@ -72,16 +87,27 @@ class Dialog(tk.Tk):
 
         self.entry.delete(0, tk.END)
 
+    def set_tag(self, widget, tag, line, beg, end):
+        widget.tag_add(tag, str(line + 1) + "." + str(beg), str(line + 1) + "." + str(end))
+
     def add_log_entry(self, summary, details, kind):
         if kind == ITEM_TO_HIGHLIGHT:
             self.item_to_highlight = summary
+            return
+
+        summary = summary.replace("\n", " ")
 
         self.log.insert('end', summary + "\n")
-        self.lines2details[self.cur_line] = details
-        self.cur_line += 1
+        self.lines2details[self.cur_log_line] = details
+        self.cur_log_line += 1
 
         if kind != NORMAL_TEXT:
-            self.log.tag_add("color_tag", str(self.cur_line) + ".0", str(self.cur_line) + "." + str(len(summary)))
+            self.log.tag_add("color_tag", str(self.cur_log_line) + ".0", str(self.cur_log_line) + "." + str(len(summary)))
+
+            self.chat.insert('end', "Chatbot: " + summary + "\n")
+            self.set_tag(self.chat, 'bold_tag', self.cur_chat_line, 0, 8)
+            self.lines2details[-self.cur_chat_line] = details
+            self.cur_chat_line += 1
 
         self.update()
 
@@ -95,8 +121,15 @@ class Dialog(tk.Tk):
         self.update()
 
     def show_details(self, event):
-        index = self.log.index(tk.CURRENT)
+        if event.widget == self.log:
+            index = self.log.index(tk.CURRENT)
+        else:
+            index = self.chat.index(tk.CURRENT)
+
         line_number = int(index.split('.')[0]) - 1
+
+        if event.widget == self.chat:
+            line_number = -line_number
 
         # Check if the item clicked is a parent item
         if line_number in self.lines2details:
